2508b4fd49baecaf97f2a3230dc5715783428a89
miyoung12 <miseja3@gmail.com>
2024-01-05 15:24:47 +0900

    [set:프론트 초기 세팅]

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <h1 className="text-3xl font-bold text-blue-600">Next-Page</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
