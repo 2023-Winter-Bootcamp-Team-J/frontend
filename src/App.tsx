56ea1e58cae28a51fb429cf165a57d00f5367405
brojun01 <brojun01@naver.com>
2024-01-11 03:47:47 +0900

    feat:LandingPage 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import ScenarioModal from "./components/ScenarioModal";
+import MainPage from './pages/MainPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './component/LandingPage';
+//import ThreeParticles from './component/ThreeParticles';
+import ParticleTutorial from './component/ThreeParticles';
+import ScenarioModal from './components/ScenarioModal';
 
 const App = () => {
   return (
@@ -8,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/main" element={<MainPage />} />
         <Route path="/modal" element={<ScenarioModal />} />
+        <Route path="/landingpage" element={<LandingPage />}></Route>
+        <Route path="/three" element={<ParticleTutorial />} />
       </Routes>
     </Router>
   );
