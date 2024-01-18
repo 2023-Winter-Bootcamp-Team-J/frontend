ba02ec49bda658ae65e56cabc6efe594f7f68b77
miyoung12 <miseja3@gmail.com>
2024-01-18 15:51:21 +0900

    feat: 시나리오 생성 및 조회 api 연동, 캐러셀 구현

--- src/components/ScenarioModal.tsx
+++ src/components/ScenarioModal.tsx
@@ -1,23 +1,14 @@
-import React, { useRef, useEffect, useState, ChangeEvent } from 'react';
-import axios from 'axios';
-import Carousel from '../components/ImgCarousel';
-
+import React, { useRef, useEffect, useState, ChangeEvent } from "react";
+import axios from "axios";
+import Carousel from "../components/ImgCarousel";
 interface ScenarioModalProps {
   isOpen: boolean;
   closeModal: () => void;
 }
-
 const ScenarioModal: React.FC<ScenarioModalProps> = ({
   isOpen,
   closeModal,
 }) => {
-  const imageUrls = [
-    '/asset/test.png',
-    '/asset/test2.jpeg',
-    '/asset/test3.jpeg',
-  ];
-  const modalRef = useRef<HTMLDivElement>(null);
-
   // 모달 외부를 클릭했을 때 모달을 닫도록 하는 이벤트 처리
   const handleBackgroundClick = (e: MouseEvent) => {
     // 배경 클릭 시 모달 닫기\
@@ -25,9 +16,11 @@ const ScenarioModal: React.FC<ScenarioModalProps> = ({
       closeModal();
     }
   };
-  const [user_id, setUserIdValue] = useState(9);
-  const [content, setContentValue] = useState('');
-  const [parent_story, setParentValue] = useState(-1);
+  const modalRef = useRef<HTMLDivElement>(null);
+  const [content, setContentValue] = useState("");
+  const [taskID, setTaskID] = useState("");
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [images, setImages] = useState<string[]>([]);
   const [characterCount, setCharacterCount] = useState<number>(0);
   const handleContentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const inputValue = e.target.value;
@@ -36,34 +29,34 @@ const ScenarioModal: React.FC<ScenarioModalProps> = ({
     if (inputValue.length <= 100) {
       setContentValue(inputValue);
       setCharacterCount(inputValue.length);
+      // console.log(characterCount);
     }
+  };
+  const handleClick = () => {
+    CreateScenario(); // 이미지 생성 요청
   };
   const CreateScenario = async () => {
     type ErrorType = {
       response: {
-        // data: ResponseType;
         status: number;
       };
     };
     try {
       if (!content.trim()) {
         // content가 공백인 경우 400에러 방지
-        console.log('문장을 입력하세요!');
-        alert('문장을 입력하세요!');
+        console.log("문장을 입력하세요!");
+        alert("문장을 입력하세요!");
         return;
       }
-      const response = await axios.post(`/api/v1/stories/`, {
-        user_id,
+      const response = await axios.post(`/api/v1/stories/images`, {
         content,
-        parent_story,
       });
-
-      if (response.status === 201) {
-        console.log('성공!');
+      if (response.status === 202) {
+        console.log("성공!");
         // 응답이 성공적인 경우 상태 업데이트
         setContentValue(content);
-        setUserIdValue(user_id);
-        setParentValue(response.data);
+        setTaskID(response.data.task_id);
+        console.log(response.data.task_id);
       }
     } catch (error) {
       console.error(error);
@@ -71,26 +64,66 @@ const ScenarioModal: React.FC<ScenarioModalProps> = ({
       console.log(errorObj.response.status);
     }
   };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // 이미지가 생성되었을 때만 ShowImage 함수 호출
+      if (taskID) {
+        ShowImage();
+      }
+    }, 5000);
+    const ShowImage = async () => {
+      try {
+        if (!taskID) {
+          console.log("taskID가 없습니다.");
+          return;
+        }
+        const response = await axios.get(`/api/v1/stories/images`, {
+          params: {
+            task_id: taskID,
+          },
+        });
+        console.log("Response:", response);
+        if (response.status === 200) {
+          console.log("성공!");
+          const newImageUrl = response.data.image_url.image_url;
+
+          // 이전에 생성한 이미지 배열에 추가
+          setImages((prevImages) => [...prevImages, newImageUrl]);
+
+          // setImageUrl(newImageUrl);
+          const imageUrl = response.data.image_url;
+          setImageUrl(imageUrl.image_url);
+          console.log(imageUrl.image_url);
+
+          // 이미지가 정상적으로 받아졌으므로 타이머 중지
+          clearInterval(intervalId);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+
+      console.log(images);
+    };
+    return () => clearInterval(intervalId);
+  }, [taskID]);
 
   useEffect(() => {
     if (isOpen) {
       // 모달이 열릴 때 외부 클릭 이벤트 리스너 등록
-      document.addEventListener('mousedown', handleBackgroundClick);
+      document.addEventListener("mousedown", handleBackgroundClick);
     } else {
       // 모달이 닫힐 때 외부 클릭 이벤트 리스너 제거
-      document.removeEventListener('mousedown', handleBackgroundClick);
+      document.removeEventListener("mousedown", handleBackgroundClick);
     }
-
     // 컴포넌트 언마운트 시에 이벤트 리스너 정리
     return () => {
-      document.removeEventListener('mousedown', handleBackgroundClick);
+      document.removeEventListener("mousedown", handleBackgroundClick);
     };
   }, [isOpen]);
-
   return (
     <div
       className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 ${
-        isOpen ? '' : 'hidden'
+        isOpen ? "" : "hidden"
       }`}
     >
       <div
@@ -103,28 +136,27 @@ const ScenarioModal: React.FC<ScenarioModalProps> = ({
         <div className="flex flex-col w-full h-[395px] justify-center items-center gap-[10px] bg-black border-2 border-white text-white">
           <div className="flex justify-center w-full h-[270px] gap-[80px]">
             <div className="w-[300px]">
-              <Carousel images={imageUrls} />
+              {imageUrl && <Carousel images={images} />}
             </div>
-
             <div className="flex flex-col justify-center w-[300px] gap-[17px] text-center">
               <div className="text-[18px] text-white">
                 시나리오 시작하기 (제목)
               </div>
               <textarea
-                // type="text"
                 placeholder="문장을 입력하세요."
-                className="h-[150px] p-[10px]  border-dashed border-2 border-white bg-transparent text-white"
+                className="h-[140px] p-[5px] mb-[20px] border-dashed border-2 border-white bg-transparent text-white"
                 value={content}
+                // onChange={(e) => setContentValue(e.target.value)}
                 onChange={handleContentChange}
                 maxLength={100}
-                style={{ resize: 'none' }}
+                style={{ resize: "none" }}
               ></textarea>
               <div className=" flex flex-col items-end  text-white top-10 ">
                 {characterCount}/{100}
               </div>
               <button
                 className="text-center w-full h-[30px] bg-green-400 border-2 border-gray-500 text-black hover:bg-blue-600 hover:text-white hover:shadow-blue-600"
-                onClick={CreateScenario}
+                onClick={handleClick}
               >
                 사진 생성하기
               </button>
@@ -141,5 +173,4 @@ const ScenarioModal: React.FC<ScenarioModalProps> = ({
     </div>
   );
 };
-
 export default ScenarioModal;
