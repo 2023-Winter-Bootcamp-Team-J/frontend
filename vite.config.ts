a39059e5159635781f22a7ce387c277a778ea4c6
hana <hana8145@gmail.com>
2024-01-23 03:34:04 +0900

    setting: vite.config.ts build 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,6 @@ import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["three"],
-    },
-  },
   plugins: [react(), svgrPlugin(), vue()],
   server: {
     port: 3000,
