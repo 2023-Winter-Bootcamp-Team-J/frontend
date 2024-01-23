929b30135de8824c4260afd028c9afbfb0c4ffae
hana <hana8145@gmail.com>
2024-01-23 23:21:34 +0900

    feat:새로고침 시 닉네임 저장 기능 구현

--- src/recoil/atoms.tsx
+++ src/recoil/atoms.tsx
@@ -1,5 +1,8 @@
 import { atom } from "recoil";
 
+import { recoilPersist } from "recoil-persist"; // 새로고침 시에도 유지
+const { persistAtom } = recoilPersist();
+
 interface UserState {
   user_id: number;
   nickname: string;
@@ -11,4 +14,5 @@ export const userState = atom<UserState>({
     user_id: 0,
     nickname: "",
   },
+  effects_UNSTABLE: [persistAtom],
 });
